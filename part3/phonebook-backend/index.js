4fbc14b2fa4dcbd7e9b39428f25039d0db7be443
alex-fedorenk0 <alex-fedorenk0>
2022-09-16 10:10:07 +0300

    ex 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -64,6 +64,12 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello</h1>')
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
