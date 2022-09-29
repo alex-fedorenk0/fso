4ff4d5ad827ee15c83880f31eed1986557570b4f
alex-fedorenk0 <alex-fedorenk0>
2022-09-29 15:32:21 +0300

    ex 3.13 fetching from monogdb

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+const Person = require('./models/person')
+
 const express = require('express')
 const app = express()
 
@@ -11,37 +15,37 @@ const morgan = require('morgan')
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })  
 })
 
 const getTimestamp = (request, response, next) => {
   const timestamp = new Date()
-  // console.log(timestamp)
-  // console.log(timestamp.toString())
   request.getTimestamp = timestamp.toString()
   next()
 }
@@ -55,14 +59,17 @@ app.get('/info', (request, response) => {
   response.end()
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(204).end()
-  }
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
+  // const id = Number(req.params.id)
+  // const person = persons.find(person => person.id === id)
+  // if (person) {
+  //   res.json(person)
+  // } else {
+  //   res.status(204).end()
+  // }
 })
 
 app.put('/api/persons/:id', (request, response) => {
